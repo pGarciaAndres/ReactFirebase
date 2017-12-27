f199a887a9f6d0ce61442a8778380fe9c4461923
Pablo Garcia <garciandres.15@gmail.com>
2017-12-27 18:01:22 +0100

    Working with firebase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,78 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    //In React, components have state!
+    //React state: If change, the component will be reloaded! (Interesting)
+    this.state = {
+      user: null
+    };
+
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  //Life cicle method launched when component is rendered in DOM (Firebase Listener).
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      // this.setState({
+      //   user: user
+      // });
+
+      //Awesome ES6 Syntax: If user == new user, will not change and eoc will be overwrited.
+      this.setState({ user });
+    });
+  }
+  
+  handleAuth() {
+    //Google login provider
+    const provider = new firebase.auth.GoogleAuthProvider();
+    //This return a promise...
+    firebase.auth().signInWithPopup(provider)
+      //Now we are using ES6 (ArrowFunction & TemplateString)
+      .then(result => console.log(`${result.user.email} has logged in!`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  handleLogout() {
+    firebase.auth().signOut()
+      .then(result => console.log(`${result.user.email} has logged out!`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  renderLoginButton() {
+    //If user is logged
+    if (this.state.user) {
+      return (
+        <div>
+          <img className="Url-photo" src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <p>Welcome {this.state.user.displayName}!</p>
+          <button onClick={this.handleLogout}>Log out</button>
+        </div>
+      );
+    } else {
+      return (
+      //If user is not logged
+      <button onClick={this.handleAuth}>Log in</button>
+      );
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React & Firebase</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          { this.renderLoginButton() }
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+//Firebase configuration
+firebase.initializeApp({
+    apiKey: "AIzaSyAvx2aAGLyy7fwebLHNakrUrarTVp-j-xw",
+    authDomain: "reactfirebase-d23fe.firebaseapp.com",
+    databaseURL: "https://reactfirebase-d23fe.firebaseio.com",
+    projectId: "reactfirebase-d23fe",
+    storageBucket: "reactfirebase-d23fe.appspot.com",
+    messagingSenderId: "806980862354"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
