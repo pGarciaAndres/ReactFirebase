38ae7a350a99eca74aec29a9ce7a3d2814513a35
Pablo Garcia <garciandres.15@gmail.com>
2018-01-18 16:53:46 +0100

    Correct components distribution App - ImageGallery - FileUpload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
-import FileUpload from './FileUpload';
+// import FileUpload from './FileUpload';
+import ImageGallery from './ImageGallery';
 import logo from './logo.svg';
 import admin from './admin.png';
 import './App.css';
@@ -19,7 +20,6 @@ class App extends Component {
 
     this.handleAuth = this.handleAuth.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
-    this.handleUpload = this.handleUpload.bind(this);
   }
 
   //Life cicle method launched when component is rendered in DOM (Firebase Listener).
@@ -30,12 +30,6 @@ class App extends Component {
       this.setState({ 
         user: user,
         error: null
-      });
-    });
-
-    firebase.database().ref('images').on('child_added', snapshot => {
-      this.setState({
-        images: this.state.images.concat(snapshot.val())
       });
     });
   }
@@ -64,73 +58,26 @@ class App extends Component {
       .catch(error => console.log(`Error ${ error.code }: ${ error.message }`));
   }
   
-  handleUpload(event) {
-    if (event.target.files.length) {
-        //Get the file from the event.
-        const file = event.target.files[0];
-        //Receive the reference.
-        const storageRef = firebase.storage().ref(`/images/${file.name}`);
-        //Task to upload the file to Firebase.
-        const task = storageRef.put(file);
-        //Firebase utility to receive the file status.
-        task.on('state_changed', snapshot => {
-            let percentage = (snapshot.bytesTransferred /snapshot.totalBytes) * 100;
-            this.setState({
-                uploadValue: percentage
-            })
-        }, error => { 
-            console.log(error.message);
-        }, () => { //Image already uploaded.
-            const record = {
-              id: task.snapshot.metadata.generation,
-              name: task.snapshot.metadata.name,
-              image: task.snapshot.downloadURL
-            };
-
-            const dbRef = firebase.database().ref('images');
-            const newImage = dbRef.push();
-            newImage.set(record);
-        });
-    }
-  }
-  
   renderLoginButton() {
+    //Check login status
     if (this.state.user) {
-      //If user is logged
       return (
-        <div>
-          <div>
-            <img className="Url-photo" src={ admin } alt={ this.state.user.email } />
-            <span>Welcome Administrator!</span>
-            <button className="logout" onClick={ this.handleLogout }>Log out</button>
-          </div>
-          <FileUpload onUpload={ this.handleUpload } />
-          {
-            this.state.images.map(image => (
-              <img className="image-gallery" src={ image.image } key={ image.id } alt=""/>
-            )).reverse()
-          }
+        <div className="login">
+          <img className="Url-photo" src={ admin } alt={ this.state.user.email } />
+          <span>Welcome Administrator!</span>
+          <button className="logout" onClick={ this.handleLogout }>Log out</button>
         </div>
       );
     } else {
-      //If user is not logged
       return (
-        <div>
-          <div>
-            <em htmlFor="password">{ this.state.error }<br/>
-            <input type="password" placeholder="Password" id="password" ref="password"/></em>
-            <button onClick={ this.handleAuth }>Admin</button>
-          </div>
-          {
-            this.state.images.map(image => (
-              <img className="image-gallery" src={ image.image } key={ image.id } alt=""/>
-            )).reverse()
-          }
+        <div className="login">
+          <em htmlFor="password">{ this.state.error }<br/>
+          <input type="password" placeholder="Password" id="password" ref="password"/></em>
+          <button onClick={ this.handleAuth }>Admin</button>
         </div>
       );
     }
   }
-
 
   render() {
     return (
@@ -141,6 +88,7 @@ class App extends Component {
         </header>
         <div className="App-intro">
           { this.renderLoginButton() }
+          <ImageGallery user={ this.state.user }/>
         </div>
       </div>
     );
--- src/ImageGallery.js
+++ src/ImageGallery.js
@@ -1,14 +1,72 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
+import FileUpload from './FileUpload';
+import './ImageGallery.css';
 
 class ImageGallery extends Component {
     constructor() {
         super();
+
+        this.state = {
+            images: [] 
+        };
+
+        this.handleUpload = this.handleUpload.bind(this);
     }
+
+    componentWillMount() {
+        firebase.database().ref('images').on('child_added', snapshot => {
+            this.setState({
+                images: this.state.images.concat(snapshot.val())
+            });
+        });
+    }
+
+    renderFileUploadButton() {
+        if (this.props.user) {
+            return (
+                <FileUpload onUpload={ this.handleUpload } />
+            );
+        }
+    }
+
+    handleUpload(event) {
+        if (event.target.files.length) {
+          //Get the file from the event.
+          const file = event.target.files[0];
+          //Receive the reference.
+          const storageRef = firebase.storage().ref(`/images/${file.name}`);
+          //Task to upload the file to Firebase.
+          const task = storageRef.put(file);
+          //Firebase utility to receive the file status.
+          task.on('state_changed', snapshot => {
+              let percentage = (snapshot.bytesTransferred /snapshot.totalBytes) * 100;
+              this.setState({
+                  uploadValue: percentage
+              })
+          }, error => { 
+              console.log(error.message);
+          }, () => { //Image already uploaded.
+              const record = {
+                id: task.snapshot.metadata.generation,
+                name: task.snapshot.metadata.name,
+                image: task.snapshot.downloadURL
+              };
+    
+              const dbRef = firebase.database().ref('images');
+              const newImage = dbRef.push();
+              newImage.set(record);
+          });
+        }
+      }
 
     render() {
         return(
-            <div>
-
+            <div className="image-gallery">
+                { this.renderFileUploadButton() }
+                { this.state.images.map(image => ( 
+                    <img className="image" src={ image.image } key={ image.id } alt=""/> 
+                  )).reverse() }
             </div>
 
         );
