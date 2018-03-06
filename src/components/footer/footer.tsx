5c0b65fee7458d635f630d690ff30cf2191b8dab
Pablo Garcia <garciandres.15@gmail.com>
2018-03-06 12:36:01 +0100

    Footer content

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 const classNames = require('./footer.css');
+const logo = require('../../images/logo.png');
 
 interface Props {
 
@@ -9,7 +10,8 @@ export const Footer = (props : Props) => {
     return (
         <div>
             <footer className={classNames.footer}>
-
+                <img src={logo} className={classNames.logo} alt="logo" />
+                <span className={classNames.title}>React & Firebase</span>
             </footer>
         </div>
     );
