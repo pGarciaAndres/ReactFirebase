77eb3e83f6c03ba8f5cd143da06452d5d18b09b9
Pablo Garcia Andres <p.garciaandres@douglas.de>
2019-01-31 11:47:15 +0100

    Go to the gallery

--- src/components/body/home/home.tsx
+++ src/components/body/home/home.tsx
@@ -10,6 +10,7 @@ export const Home = (props : Props) => {
             <div className={classNames.home}>
                 <h2>Home</h2>
             </div>
+            <h3>Psst! Go to the gallery</h3>
         </div>
     );
 }
